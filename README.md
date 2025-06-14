eec05fa425348cedf70a967d0a037fc2d8ee13cb
Yogesh Yadav <yogesh7132@gmail.com>
2025-06-14 10:37:38 +0530

    genesis

---
+++ README.md
@@ -0,0 +1,17 @@
+## CodeScoop App
+
+A Scoop on Freeware & Open Source Resources...
+
+CodeScoop.dev is an initiative to encourage the exchange of open educational resources (OER) allowing free access, reuse, reassignment, adaptation and redistribution by others. All the resources listed below is a volunteer effect and you are free to copy, distribute and/or modify it, under the terms of the GNU General Public License (GPLv3) license published by the Free Software Foundation.
+
+For development sources, documentation and other information, view public source
+
+<hr>
+
+NOTE: To provide feedback, report a bug or request an enhancement, please use the github issue tracker. Or you can send an e-mail.
+
+>> We invites volunteers who are interested in working with others to put their technology skills to good use for social change.
+
+
+
+
---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "CodeScoop | Learn programming skills by working on real-world projects.",
+  description: "CodeScoop.dev is an initiative to empower communities through Open Source & Education.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
